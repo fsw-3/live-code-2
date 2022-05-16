8c9caeec10904498759f20e78ebd6b1c6aadc5e4
daengearly <ewako.mks13@gmail.com>
2022-05-16 21:33:44 +0700

    Endpoint /docs

--- app/index.js
+++ app/index.js
@@ -8,6 +8,26 @@ const morgan = require("morgan");
 const router = require("../config/routes");
 
 const app = express();
+const PORT = 8000;
+
+swaggerDocument = require("../swagger.json");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Data Management API",
+      version: "1.0.0",
+    },
+  },
+  // List of files to be processes. You can also set globs './routes/*.js"
+  apis: ["../config/routes.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
+// Swagger API Docs
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 /** Install request logger */
 app.use(morgan("dev"));
@@ -17,5 +37,9 @@ app.use(express.json());
 
 /** Install Router */
 app.use(router);
+
+app.listen(PORT, () => {
+  console.info(`Server running at http://localhost:${PORT}`);
+});
 
 module.exports = app;
--- config/routes.js
+++ config/routes.js
@@ -17,6 +17,251 @@ apiRouter.delete(
 );
 
 /**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
+apiRouter.get("/", (request, response) => {
+  response.json("Index page");
+});
+// ===============================================
+
+/**
+ * @swagger
+ * definitions:
+ *   Register:
+ *     required:
+ *       - email
+ *       - password
+ *     properties:
+ *       email:
+ *         type: string
+ *       password:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * definitions:
+ *   Login:
+ *     required:
+ *       - email
+ *       - password
+ *     properties:
+ *       email:
+ *         type: string
+ *       password:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * definitions:
+ *   Create_Update_Data:
+ *     required:
+ *       - nama
+ *       - jenis
+ *       - foto
+ *     properties:
+ *       nama:
+ *         type: string
+ *       jenis:
+ *         type: string
+ *       foto:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   - name: Auth
+ *     description: Authenticate User
+ *   - name: Data
+ *     description: Data
+ */
+
+// AUTH ENDPOINT
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     description: Register user
+ *     tags: [Auth]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: email
+ *         description: User's email.
+ *         required: true
+ *         in: formData
+ *         type: string
+ *       - name: password
+ *         description: User's password.
+ *         required: true
+ *         type: string
+ *     responses:
+ *       201:
+ *         description: register
+ *         schema:
+ *           type: object
+ *           $ref: '#/definitions/Register'
+ */
+// apiRouter.post('/auth/signup', userController.createNewUserApi);
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     description: Login to app
+ *     tags: [Auth]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: email
+ *         description: User's email.
+ *         required: true
+ *         in: formData
+ *         type: string
+ *       - name: password
+ *         description: User's password.
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: login
+ *         schema:
+ *           type: object
+ *           $ref: '#/definitions/Login'
+ */
+// apiRouter.post("/auth/login", userController.signUserApi);
+
+//============================================================
+
+// DATA ENDPOINT
+/**
+ * @swagger
+ * /find-all:
+ *   get:
+ *     description: Get all data
+ *     tags: [Data]
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: data
+ */
+// router.get('/find-all', authMiddleware.authorizationToken, dataController.findAllDataApi);
+
+/**
+ * @swagger
+ * /findbyid/:id:
+ *   get:
+ *     description: Get data by id
+ *     tags: [Data]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *       - type: string
+ *     responses:
+ *       200:
+ *         description: data by id
+ */
+// apiRouter.get('/findbyid/:id', authMiddleware.authorizationToken, dataController.findDataByIdApi);
+
+/**
+ * @swagger
+ * /create:
+ *   post:
+ *     description: Create new data
+ *     tags: [Data]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: nama
+ *         description: Data's name.
+ *         required: true
+ *         in: formData
+ *         type: string
+ *       - name: jenis
+ *         description: Data's type.
+ *         required: true
+ *         in: formData
+ *         type: string
+ *       - name: foto
+ *         description: Data's photo.
+ *         required: true
+ *         in: formData
+ *         type: string/file
+ *     responses:
+ *       201:
+ *         description: create data
+ *         schema:
+ *           type: object
+ *           $ref: '#/definitions/Create_Update_Data'
+ */
+// apiRouter.post('/create', authMiddleware.authorizationToken, dataController.createNewDataApi);
+
+/**
+ * @swagger
+ * /update/:id:
+ *   put:
+ *     description: Update data by id
+ *     tags: [Data]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Data's id.
+ *         required: true
+ *         in: formData
+ *         type: string
+ *       - name: nama
+ *         description: Data's name.
+ *         required: true
+ *         in: formData
+ *         type: string
+ *       - name: jenis
+ *         description: Data's type.
+ *         required: true
+ *         in: formData
+ *         type: string
+ *       - name: foto
+ *         description: Car's photo.
+ *         required: true
+ *         in: formData
+ *         type: string/file
+ *     responses:
+ *       200:
+ *         description: update data
+ *         schema:
+ *           type: object
+ *           $ref: '#/definitions/Create_Update_Data'
+ */
+// apiRouter.put('/update/:id', authMiddleware.authorizationToken, dataController.updateDataApi);
+
+/**
+ * @swagger
+ * /delete/:id:
+ *   delete:
+ *     description: Delete data by id
+ *     tags: [Data]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *       - type: string
+ *     responses:
+ *       200:
+ *         description: data by id
+ */
+// apiRouter.delete('/delete/:id', authMiddleware.authorizationToken, dataController.deleteData);
+
+/**
  * TODO: Delete this, this is just a demonstration of
  *       error handler
  */
